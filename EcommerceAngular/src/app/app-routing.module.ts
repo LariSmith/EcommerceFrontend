94e0f96ff0e091a9fb31ecc405a731c8e8285fa9
LariSmith <lena.dracarys@gmail.com>
2021-05-16 17:48:58 -0300

    criando componentes

--- EcommerceAngular/src/app/app-routing.module.ts
+++ EcommerceAngular/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProdutosComponent } from './produtos/produtos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'produtos', pathMatch: 'full' },
+  { path: 'produtos', component: ProdutosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- EcommerceAngular/src/app/app.module.ts
+++ EcommerceAngular/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutosComponent,
+    HeaderComponent,
+    FooterComponent,
+    CarrinhoComponent,
+    PedidosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
