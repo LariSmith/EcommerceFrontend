cb694770e03f873da5a6ac87eebc7705802f49e5
LariSmith <lena.dracarys@gmail.com>
2021-05-17 15:27:19 -0300

    implementando carrinho

--- EcommerceAngular/src/app/app-routing.module.ts
+++ EcommerceAngular/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
 import { ProdutosComponent } from './produtos/produtos.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'produtos', pathMatch: 'full' },
-  { path: 'produtos', component: ProdutosComponent }
+  { path: 'produtos', component: ProdutosComponent },
+  { path: 'carrinho/:produtos', component: CarrinhoComponent }
 ];
 
 @NgModule({
